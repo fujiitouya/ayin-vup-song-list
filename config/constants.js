c7ed6a4dd4bc0961287005b603fdebffb1d1dbf1
Fujii Touya <a6087349@outlook.com>
2023-12-30 22:28:01 +0800

    First Initial
    First Initial

---
+++ config/constants.js
@@ -0,0 +1,36 @@
+let config = {
+    Name: "阿音_Official",
+    
+    BiliLiveRoomID: "",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2023 阿音_Official 解释权归本直播间所有。",
+
+    Cursor: true,
+
+    LanguageCategories: ["国语", "外语"],
+    RemarkCategories: [],
+
+    BannerTitle: "雪域provealms所属",
+    BannerContent: [``,
+     ],
+
+    CustomButtons: [
+        {
+            link: "mailto:provealms@foxmail.com",
+            name: "商务合作",
+            image: ""
+        }
+        // {
+        //     link: "https://space.bilibili.com/3493271057730096",
+        //     name: "录播组",
+        //     image: ""
+        // }
+    ]
+}
+
+
+module.exports = {
+    config
+}
