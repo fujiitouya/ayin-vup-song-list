824cf8e62765ff431ee6bc6d7fb7e525ac3ba244
Fujii Touya <48616819+fujiitouya@users.noreply.github.com>
2024-01-06 19:06:10 +0800

    update constants.js
    update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 let config = {
-    Name: "阿音_Official",
+    Name: "阿音",
     
-    BiliLiveRoomID: "",
+    BiliLiveRoomID: "31291629",
     
     NetEaseMusicId: "",
     QQMusicId: "",
@@ -13,7 +13,18 @@ let config = {
     RemarkCategories: [],
 
     BannerTitle: "雪域provealms所属",
-    BannerContent: [``,
+    BannerContent: [`
+    ♡点歌示范→发送弹幕点歌+歌名；
+♡曲风偏国风and流行；
+♡粉丝团成员每日可点一首歌；
+♡新舰长可置顶一首非SC的歌；
+♡置顶歌曲30SC；
+♡歌单内SC歌曲30起
+♡提督点歌默认置顶；
+♡歌曲重复点双倍SC；
+♡点歌CD15min；
+♡歌曲冷却时间48h；
+`,
      ],
 
     CustomButtons: [
